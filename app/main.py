4a8409e142c0af86531fc95b24178c81d3f86928
OsaKaIchiro <e2406@s.akashi.ac.jp>
2025-03-18 16:40:43 +0900

    現段階の変更

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from app.routers import log_in_page
+from app.routers import log_in_page, ranking, home
 
 
 app = FastAPI()
 app.mount(path="/static" , app=StaticFiles(directory = "/app"), name = "static")
 app.include_router(log_in_page.router)
+app.include_router(ranking.router)
 
